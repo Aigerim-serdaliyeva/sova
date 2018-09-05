f34da8ee0ecef0250aff8414d450679ca7a866ef
Сарсенбаев Нурбол <kuma_official@mail.ru>
2018-09-05 17:35:30 +0600

    send file fixed

--- app/mail.php
+++ app/mail.php
@@ -39,6 +39,31 @@ if(isset($_POST["info"])) {
 
   mail($admin_email, $form_subject, $message, $headers);
 
+  if ($_POST['info'] == 'Получить информацию') {
+    $filename = "SovaStudio.docx";
+    $path = "./files/" . $filename;
+    $file_content = chunk_split(base64_encode(file_get_contents($path)));
+    $uid = md5(uniqid(time()));
+
+    $file_header = "From: ".adopt($project_name)." ".$server_mail."\r\n";
+    $file_header .= "Reply-To: ".$admin_email."\r\n";
+    $file_header .= "MIME-Version: 1.0\r\n";
+    $file_header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";					
+
+    // message & attachment
+    $file_message = "--".$uid."\r\n";
+    $file_message .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+    $file_message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+    $file_message .= "--".$uid."\r\n";
+    $file_message .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n";
+    $file_message .= "Content-Transfer-Encoding: base64\r\n";
+    $file_message .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+    $file_message .= $file_content."\r\n\r\n";
+    $file_message .= "--".$uid."--";
+
+    mail($_POST['email'], adopt($filename), $file_message, $file_header);
+  }
+
   header("Location: /thanks.html");
   
 } 
